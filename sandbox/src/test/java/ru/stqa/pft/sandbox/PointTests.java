03dac79b9ac4d4555d9703bacea88a1e181ec139
Lavannaleo <lavanna.leo@gmail.com>
2022-05-09 00:35:20 +0300

    Update PointTests.java

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -4,9 +4,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PointTests {
+  @Test
+  //All coordinates are equal to zero
+  public void testDistance_ZeroValues(){
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, 0);
+
+    Assert.assertEquals(p2.distance(p1), 0);
+  }
 
   @Test
-  public void testDistance(){
+  //Different coordinate values
+  public void testDistance_DifferentValues(){
     Point p1 = new Point(0.15, -89.25);
     Point p2 = new Point(-55.89, 400);
 

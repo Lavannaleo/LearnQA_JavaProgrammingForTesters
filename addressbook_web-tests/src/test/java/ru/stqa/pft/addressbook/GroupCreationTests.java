23a7f0d063f3c2a7ee673ed36f997580d794201c
Lavannaleo <lavanna.leo@gmail.com>
2022-05-14 22:44:57 +0300

    Update GroupCreationTests.java

--- addressbook_web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook_web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,12 +1,12 @@
 package ru.stqa.pft.addressbook;
 
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
 
 public class GroupCreationTests extends TestBase{
 
   @Test
-  public void testGroupCreation() throws Exception {
+  public void testGroupCreation() {
     gotoGroupPage();
     initGroupCreation();
     fillGroupForm(new GroupData("New Test", "Header", "Footer"));

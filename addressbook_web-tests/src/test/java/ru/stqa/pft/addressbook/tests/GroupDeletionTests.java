56f83b0f3fe18d0385ad7eb1cd94809beccb2688
Lavannaleo <lavanna.leo@gmail.com>
2022-09-25 19:15:51 +0300

    Lesson 5.4 - Fluent Interface for Group-tests

--- addressbook_web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook_web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -13,7 +13,7 @@ public class GroupDeletionTests extends TestBase {
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().list().size() == 0) {
-      app.group().create(new GroupData("New Test", "Header", "Footer"));
+      app.group().create(new GroupData().withName("test1'"));
     }
   }
 
--- addressbook_web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook_web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -15,7 +15,7 @@ public class GroupModificationTests extends TestBase{
   public void ensurePreconditions() {
     app.goTo().groupPage();
     if (app.group().list().size() == 0) {
-      app.group().create(new GroupData("New Test", "Header", "Footer"));
+      app.group().create(new GroupData().withName("test1"));
     }
   }
 
@@ -24,7 +24,7 @@ public class GroupModificationTests extends TestBase{
 
     List<GroupData> before = app.group().list();
     int index = before.size() - 1;
-    GroupData group = new GroupData(before.get(index).getId(),"TestGroup", "TestHeader", "TestFooter");
+    GroupData group = new GroupData().withId(before.get(index).getId()).withName("TestGroup").withHeader("TestHeader").withFooter("TestFooter");
     app.group().modify(index, group);
     List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size());

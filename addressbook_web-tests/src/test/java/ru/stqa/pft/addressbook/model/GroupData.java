597efdf8d925b7dc8ff26e464fb1aa418885fa1b
Lavannaleo <lavanna.leo@gmail.com>
2022-11-26 20:45:01 +0300

    Lesson 7.2 - ORM is configurated

--- addressbook_web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook_web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,22 +3,50 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Columns;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @XStreamAlias("group")
+@Entity
+@Table(name = "group_list")
 public class GroupData {
 
   @XStreamOmitField
+  @Id
+  @Column(name = "group_id")
   private int id = Integer.MAX_VALUE;
 
   @Expose
+  @Column(name = "group_name")
   private  String name;
   @Expose
+  @Column(name = "group_header")
+  @Type(type = "text" )
   private  String header;
   @Expose
+  @Column(name = "group_footer")
+  @Type(type = "text" )
   private  String footer;
 
 
   public int getId() { return id;  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getHeader() {
+    return header;
+  }
+
+  public String getFooter() {
+    return footer;
+  }
 
   public GroupData withId(int id) {
     this.id = id;
@@ -38,18 +66,6 @@ public class GroupData {
   public GroupData withFooter(String footer) {
     this.footer = footer;
     return this;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getHeader() {
-    return header;
-  }
-
-  public String getFooter() {
-    return footer;
   }
 
   @Override

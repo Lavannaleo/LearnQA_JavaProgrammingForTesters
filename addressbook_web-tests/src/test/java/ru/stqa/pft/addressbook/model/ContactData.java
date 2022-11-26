702caeb2c583fd5442d5268c95519e06fbdee24b
Lavannaleo <lavanna.leo@gmail.com>
2022-11-26 22:18:10 +0300

    Lesson 7.3 - conditions of data select

--- addressbook_web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook_web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,39 +3,63 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
+
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
+
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column(name = "lastname")
   private String lastname;
   @Expose
+  @Column(name = "firstname")
   private String firstname;
+  @Transient
   private String middlename;
+  @Transient
   private String nickname;
-
+  @Transient
   private String allEmails;
   @Expose
+  @Column(name = "email")
+  @Type(type = "text")
   private String email;
-
+  @Transient
   private String email2;
-
+  @Transient
   private String email3;
-
+  @Transient
    private String bday;
+  @Transient
   private String bmonth;
+  @Transient
   private String byear;
   @Expose
+  @Column(name = "address")
+  @Type(type = "text")
   private String address;
+  @Transient
    private String allPhones;
+  @Transient
   private String home;
   @Expose
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobile;
+  @Transient
   private String work;
-
-  private File photo;
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
   public int getId() { return id;  }
 
   public ContactData withId(int id) {
@@ -123,11 +147,11 @@ public class ContactData {
   }
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 

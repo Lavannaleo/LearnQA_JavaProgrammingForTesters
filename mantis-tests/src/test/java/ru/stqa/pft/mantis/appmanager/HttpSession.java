d489f15b453c8dad94187856fe8ee3271c43bfd7
Lavannaleo <lavanna.leo@gmail.com>
2022-12-13 01:08:31 +0300

    Lesson 8.3 - testLogin() errror

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -1,6 +1,16 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.omg.CORBA.NameValuePair;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +22,7 @@ public class HttpSession {
 
   public HttpSession(ApplicationManager app) {
     this.app = app;
-    httpclient = HttpClients.custom().setReirectStrategy(new LaxRedirectStrategy()).build;
+    httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
   }
 
   public boolean login(String username, String password) throws IOException {
@@ -21,10 +31,10 @@ public class HttpSession {
     params.add(new BasicNameValuePair("username", username));
     params.add(new BasicNameValuePair("password", password));
     params.add(new BasicNameValuePair("secure_session", "on"));
-    params.add(new BasicNameValuePair("return", index.php));
-    post.setEntity(new UrlEncodedFprmEntity(params));
+    params.add(new BasicNameValuePair("return", "index.php"));
+    post.setEntity(new UrlEncodedFormEntity(params));
     CloseableHttpResponse responce = httpclient.execute(post);
-    String body = geTextFromResponce(responce);
+    String body = geTextFrom(responce);
     return body.contains(String.format("<span class=\"italic\">%s</span>", username));
   }
 
@@ -39,7 +49,7 @@ public class HttpSession {
   public boolean isLoggedInAs(String username) throws IOException {
     HttpGet get = new HttpGet(app.getProperty("base.Url") + "/login.php");
     CloseableHttpResponse responce = httpclient.execute(get);
-    String body = geTextFromResponce(responce);
+    String body = geTextFrom(responce);
     return body.contains(String.format("<span class=\"italic\">%s</span>", username));
   }
 

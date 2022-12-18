829948d2f79165d3679f2b846ed211398dc1cc4f
Lavannaleo <lavanna.leo@gmail.com>
2022-12-18 13:42:03 +0300

    Lesson 8.6 - Recieve mail from embedded mail server

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/MailHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/MailHelper.java
@@ -22,7 +22,7 @@ public class MailHelper {
   public List<MailMessage> waitForMail(int count, long timeout) throws MessagingException, IOException {
     long start = System.currentTimeMillis();
     while (System.currentTimeMillis() < start + timeout) {
-      if (wiser.getMessages().size() > count) {
+      if (wiser.getMessages().size() >= count) {
         return wiser.getMessages().stream().map((m) -> toModelMail(m)).collect(Collectors.toList());
       }
       try {
